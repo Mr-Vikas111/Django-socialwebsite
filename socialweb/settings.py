93ca6ceb1273d5a44960e6417639ddcbb8dfa124
vikasminj <vikashminj1999@gmail.com>
2021-07-26 17:23:50 +0530

    setting change

--- socialweb/settings.py
+++ socialweb/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/UploadImage/'
 MEDIA_ROOT = BASE_DIR / 'UploadImage'
 
